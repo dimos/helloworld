9bdd8f87b6f113c147c5b950654f39b8a4b0cdbf
Dimos Poupos <dimospoupos@gmail.com>
2011-06-18 13:42:38 +0300

    Added a random number on line 10

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import random
 
 name = "Dimos Poupos"
 
 print("Hello World!")
 print("My name is {0} and I testing now the git!".format(name))
+print("This is a random number: {0}".format(random.randint(1, 58)))
